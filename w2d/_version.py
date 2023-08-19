b51ef730f8d39fbb085159dd9944ff77e33c1264
clach04 <clach04@gmail.com>
2023-08-19 15:34:10 -0700

    pip supported dev version

--- w2d/_version.py
+++ w2d/_version.py
@@ -1,2 +1,2 @@
-version_tuple = __version_info__ = (0, 1, 0, 'git')
+version_tuple = __version_info__ = (0, 1, 0, 'dev')
 version = version_string = __version__ = '.'.join(map(str, __version_info__))
